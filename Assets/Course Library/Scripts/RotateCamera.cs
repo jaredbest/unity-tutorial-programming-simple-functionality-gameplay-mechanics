6b3a2a56926e5357de4acb6fa8e5de0f4edf1b86
jaredbest <me@jaredbest.com>
2021-03-07 12:24:27 +0100

    Finished prototype

---
+++ Assets/Course Library/Scripts/RotateCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    float rotationSpeed = 90;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * rotationSpeed);
+    }
+}
